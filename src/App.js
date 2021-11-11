c62b8857c14c3b5ccecaf362524e632143d19512
olga <oluciams84@gmail.com>
2021-11-10 21:15:10 -0500

    Adiciona estado y funcion para negar el valor del estado para renderizado condicional

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+function App (){
+
+  const [select, setSelect] = useState(false); 
+  
+  const handleSelect = ()=>{
+    setSelect(!select)  
+
+  }  
     return (
       <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
+        <label><input onClick={handleSelect} type="checkbox" /> Mostrar información importante</label>
+        {select ? 
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        :
+        <p></p>
+        }
       </div>
     );
-  }
+  
 }
 
 export default App;
